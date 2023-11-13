98c42cd8a0de66a71f9fc9b8ba06ed4186e140e5
donny_444 <104888835+donny444@users.noreply.github.com>
2023-11-13 21:54:31 +0700

    Add files via upload

--- nodejs-mongoose/model/animal_schema.js
+++ nodejs-mongoose/model/animal_schema.js
@@ -1,23 +1,18 @@
 const mongoose = require("mongoose");
 
-// define a schema
 const animalSchema = new mongoose.Schema({ name: String, type: String },
 {
-    // Assign a function to the "methods" object of our animalSchema through schema options
-    // By following this approach, there is no need to create a seperate TS type to define the type of the intance functions.
     methods: {
         findSimilarTypes(cb) {
             return mongoose.model('Aninmal').find({ type: this.type }, cb);
         }
+    },
+    statics: {
+        findByName(name) {
+            return this.find({ name: new RegExp(name, "i")});
+        }
     }
 });
-
-// Or, assign a function to the "methods" object of our animalSchema
-/*
-animalSchema.methods.findSimilarTypes = function(cb) {
-    return mongoose.model("Animal").find({ type: this.type }, cb);
-};
-*/
 
 const Animal = mongoose.model("Animal", animalSchema);
 const dog = new Animal({ type: "dog" });
@@ -26,4 +21,5 @@ dog.findSimilarTypes((err, dogs) => {
     console.log(dogs); // woof
 })
 
+let animals = await Animal.findByName("fido");
 module.exports = Animal;
--- nodejs-mongoose/model/schema.js
+++ nodejs-mongoose/model/schema.js
@@ -6,8 +6,8 @@ schema.path("_id");
 const Model = mongoose.model("Test", schema);
 
 const doc = new Model();
-doc._id instanceof mongoose.Types.ObjectId; // true
-await doc.save(); // Throws "document must have an _id before saving"
+doc._id instanceof mongoose.Types.ObjectId;
+await doc.save();
 
 doc._id = 1;
-await doc.save(); // works
+await doc.save();
