57de4efb0dd3337c2267e42cbe4f411b4b3cd445
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-22 16:27:48 +0700

    Add files via upload

--- blog-post/backend/app.js
+++ blog-post/backend/app.js
@@ -13,7 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/blog-post");
+mongoose.connect("mongodb://127.0.0.1:27017/blog-post").
+    catch(error => console.log("Error connecting to database"));
+
+mongoose.connection.on("connected", () => console.log("Connected to database"));
 
 const routes = require("./router/routes.js");
 
