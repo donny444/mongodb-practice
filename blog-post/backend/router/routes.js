f78b6e6bb3986d9b9024029ca5579a294f15f402
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-22 16:28:25 +0700

    Add files via upload

--- blog-post/backend/router/routes.js
+++ blog-post/backend/router/routes.js
@@ -2,8 +2,10 @@ const express = require("express");
 const route = express.Router();
 const blogController = require("../controller/blog_controller.js");
 const postController = require("../controller/post_controller.js");
+const commentController = require("../controller/comment_controller.js");
 
 route.get("/", blogController);
 route.post("/post", postController);
+route.get("/comment", commentController);
 
 module.exports =  route;
